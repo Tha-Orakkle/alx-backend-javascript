bf49ca5e428dfd8b629cd924ed1efeb4d856c812
Tha-Orakkle <adegbiranayinoluwa.paul@yahoo.com>
2023-12-23 06:57:51 +0100

    Handle multiple successful promises

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,14 @@
+import { createUser, uploadPhoto } from './utils';
+
+function handleProfileSignup() {
+  return Promise.all([createUser(), uploadPhoto()])
+    .then((values) => {
+        const firstName = values[0].firstName;
+        const lastName = values[0].lastName;
+        const body = values[1].body;
+        console.log(`${body} ${firstName} ${lastName}`);
+    })
+    .catch(() => console.log("Signup system offline"));
+}
+
+export default handleProfileSignup;
