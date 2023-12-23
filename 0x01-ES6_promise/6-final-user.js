761d8447223eb75ea216e165adcc7ee004da24ec
Tha-Orakkle <adegbiranayinoluwa.paul@yahoo.com>
2023-12-23 08:09:59 +0100

    Handle multiple promises

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,16 @@
+import signUpUser from "./4-user-promise";
+import uploadPhoto from "./5-photo-reject";
+
+function handleProfileSignup(firstName, lastName, fileName) {
+  const promises = [
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]
+  return Promise.allSettled(promises)
+    .then((res) => res.map((data) => ({
+      status: data.status,
+      value: data.status === 'fulfilled' ? data.value : data.reason,
+    })));
+}
+
+export default handleProfileSignup;
