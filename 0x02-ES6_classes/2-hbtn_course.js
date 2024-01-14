cef9c2b1e7afa1699021b3583b8a09225ace1e26
Tha-Orakkle <adegbiranayinoluwa.paul@yahoo.com>
2024-01-14 02:19:18 +0100

    A Course, Getters, and Setters

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -36,8 +36,8 @@ export default class HolbertonCourse {
   }
 
   set length(value) {
-    if (typeof length === 'number') {
-      this._length = length;
+    if (typeof value === 'number') {
+      this._length = value;
     } else {
       throw new TypeError('Length must be a number');
     }
@@ -48,8 +48,8 @@ export default class HolbertonCourse {
   }
 
   set students(value) {
-    if (Array.isArray(students)) {
-      this._students = students;
+    if (Array.isArray(value)) {
+      this._students = value;
     } else {
       throw new TypeError('Students must be an array');
     }
