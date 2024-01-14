36d6623a3604cb8ed74b39b38f55e6c056b59d61
Tha-Orakkle <adegbiranayinoluwa.paul@yahoo.com>
2024-01-14 02:39:28 +0100

    ES lint Compliant

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -4,14 +4,13 @@ export default class Building {
     if (this.constructor !== Building && typeof this.evacuationWarningMessage !== 'function') {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
-   }
+  }
 
   get sqft() {
-    return this.sqft;
+    return this._sqft;
   }
 
   set sqft(value) {
     this._sqft = value;
   }
-
 }
