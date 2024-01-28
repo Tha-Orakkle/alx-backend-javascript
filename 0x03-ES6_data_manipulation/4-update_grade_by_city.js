b5fe78d5304cf2729f234b5e5cfc2e22fa7e41d7
Tha-Orakkle <adegbiranayinoluwa.paul@yahoo.com>
2024-01-28 20:21:30 +0100

    Combine

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,10 @@
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  return students.filter(({ location }) => location === city)
+    .map((student) => {
+      const gradeObj = newGrades.find(
+        (grade) => grade.studentId === student.id
+        );
+      const grade = gradeObj ? gradeObj.grade : 'N/A';
+      return { ...student, grade };
+    });
+}
