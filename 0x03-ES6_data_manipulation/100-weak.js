f123b81c7ad9e138691c68f2531983a70d4db006
Tha-Orakkle <adegbiranayinoluwa.paul@yahoo.com>
2024-02-05 02:44:41 +0100

    Weak link data structure

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,11 @@
+export const weakMap = new WeakMap();
+
+export function queryAPI(endpoint) {
+    let count;
+
+    count = weakMap.get(endpoint) || 0;
+
+    if (count + 1 >= 5) throw new Error('Endpoint load is high');
+
+    weakMap.set(endpoint, count + 1);
+}
